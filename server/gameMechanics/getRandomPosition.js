c2063337c7ca48bc3cabfb1cd50ca2e7a07dce3b
Artem <artem@enkonix.com>
2024-03-26 14:47:07 +0100

    Tiny fix

--- server/gameMechanics/getRandomPosition.js
+++ server/gameMechanics/getRandomPosition.js
@@ -1,10 +1,12 @@
-const { gameSpace } = require('../../config/gameSettings');
+const { gameSpace: defaultGameSpace } = require('../../config/gameSettings');
 
 /**
  * Generates a random position within the game space.
  * @returns {{x: number, y: number}} Position object.
  */
 function getRandomPosition() {
+  const gameSpace = global.gameSpace || defaultGameSpace; // Use global.gameSpace if defined, otherwise fallback to default from gameSettings
+
   try {
     const position = {
       x: Math.floor(Math.random() * gameSpace.width),
